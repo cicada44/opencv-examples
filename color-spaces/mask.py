bcd8654e14509a1dbb3117d1911927f01286b791
Cicada <hubbleadd@bk.ru>
2023-06-24 14:43:07 +0700

    Color spaces: Add examples

---
+++ color-spaces/mask.py
@@ -0,0 +1,23 @@
+# -----------------------------------------------------------------------------
+#
+# Example for highlight blue shades from RGB on Python3
+#
+# -----------------------------------------------------------------------------
+
+import numpy as np
+import cv2
+
+imageRGB = cv2.imread("image.png")
+
+if imageRGB is None:
+    exit(-1)
+
+lowerBlue = np.array([15, 15, 0])
+print(lowerBlue)
+upperBlue = np.array([130, 170, 255])
+
+mask = cv2.inRange(imageRGB, lowerBlue, upperBlue)
+
+blueRegions = cv2.bitwise_and(imageRGB, imageRGB, mask=mask)
+
+cv2.imwrite("imageBlue.png", blueRegions)
---
+++ color-spaces/rgb-to-hsv.py
@@ -0,0 +1,16 @@
+# -----------------------------------------------------------------------------
+#
+# Example for translation image from RGB to HSV color space on Python3
+#
+# -----------------------------------------------------------------------------
+
+import cv2
+
+imageRGB = cv2.imread("image.png");
+
+if imageRGB is None:
+    exit(-1)
+
+imageHSV = cv2.cvtColor(imageRGB, cv2.COLOR_RGB2HSV);
+
+cv2.imwrite("imageHSV.png", imageHSV);
